1ca9dfa249ff5d7a2a4d986228251299b2383da4
Knoester <ewout.knoester@wur.nl>
2021-05-06 16:11:35 +0300

    Minor adjustments to cleaning of data: -> Create more understandable treatment names -> Updated size categories (now roughly 5 per tree-species-size group) -> Removed ".add" from drop column formula: this gave an error?
    Still to be addressed:
    -> Cause death sometimes empty, sometimes NA: have uniform notation?
    -> Notation EV varies (scientific notation for first dozen of observations?)
    
    Thoughts:
    -> Fused fragments: useful for survival data, but unusable for growth
    -> Dead fragments: what to do with SGR??
    -> Already exlude Exclude & Missing frags?

--- cleaning_exp.R
+++ cleaning_exp.R
@@ -1,3 +1,13 @@
+# ------------------------------------------
+# Cleaning experiment
+# Hamadi Mwamlavya, Ewout Knoester
+# Created 24-Mar-2021
+# ------------------------------------------
+
+# Set R and packages
+rm(list=ls()) # Clear workspace
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path)) #Set directory at current directory
+
 library(readxl) # Import excel sheets
 library(openxlsx) # Create excel sheets
 library(tidyverse) # tidy data
@@ -13,16 +23,16 @@ raw.data.1 <- as.data.frame(append(raw.data.1,
 
 # Fill in the newly created variable 'Size' with conditional values from 'Length'
 raw.data.1$Size <- ifelse(
-  raw.data.1$Length.S1 < 5, "Small",
-  ifelse(raw.data.1$Length.S1 < 10, "Medium","Large"))
+  raw.data.1$Length.S1 < 3.4, "Small",
+  ifelse(raw.data.1$Length.S1 < 6.4, "Medium","Large"))
 
 # Fill in the newly created variable 'Treatment' with conditional values from 'Structure'
 # There's need to identify the control group i.e. trees with no cleaning
 
 raw.data.1$Treatment <- ifelse(
-  raw.data.1$Structure %in% c("Tree 095", "Tree 089","Tree 079"),"Treatment_1",
-  ifelse(raw.data.1$Structure %in% c("Tree 092", "Tree 093", "Tree 088"), "Treatment_2",
-         ifelse(raw.data.1$Structure %in% c("Tree 099", "Tree 098", "Tree 094"), "Treatment_3","Control")))
+  raw.data.1$Structure %in% c("Tree 095", "Tree 089","Tree 079"),"Weekly",
+  ifelse(raw.data.1$Structure %in% c("Tree 092", "Tree 093", "Tree 088"), "Monthly",
+         ifelse(raw.data.1$Structure %in% c("Tree 099", "Tree 098", "Tree 094"), "Quarterly","Never")))
 
 # Convert to dataframe for use with dplyr
 raw.data.2 <- as.data.frame(
@@ -31,7 +41,7 @@ raw.data.2 <- as.data.frame(
 # Drop some columns
 
 clean.data <- raw.data.2 %>%
-  select(-c("Origin","Batch", "Select","Alive","Condition","Comments",".add")) 
+  select(-c("Origin","Batch", "Select","Alive","Condition","Comments")) 
 
 # Create a new excel sheet
 
